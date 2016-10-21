1c9b029278e9bd1352552e56354d6a1ec73267bf
xuesedianfeng <87778934@163.com>
2016-10-21 22:38:00 +0800

    调整连接结构

--- src/script/StartScript.java
+++ src/script/StartScript.java
@@ -1,9 +1,6 @@
 package script;
 
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * @author huangliy
@@ -11,24 +8,13 @@ import java.net.Socket;
  */
 public class StartScript {
 	public static void main(String[] args) throws Throwable {
-		Socket socket = new Socket("106.75.6.79", 8210);
-		
-		 // 向服务器端发送数据  
-        OutputStream os =  socket.getOutputStream();  
-        DataOutputStream bos = new DataOutputStream(os);  
-        // 创建一个数据包
-        ReignSendPack pack = new ReignSendPack("reconnect", "sessionId=heihei", true);
-        bos.write(pack.getSendData());
-        bos.flush();  
-
-        // 接收服务器端数据  
-        InputStream in = socket.getInputStream();  
-        ReignBackPack rbp = new ReignBackPack(in, true);
-        String content = rbp.getContent();
-        System.out.println(content);
-        
-        bos.close();
-        in.close();
-        socket.close();
+		ReignConnection connect = new ReignConnection("106.75.6.79", 8210);
+		String rtn = connect.sendWithEcho("reconnect", "sessionId=81B3811AAD3203A135BA2FB9A58F58BE");
+		JSONObject json = JSONObject.parseObject(rtn);
+		int state = json.getIntValue("state");
+		if (state == 1) {
+			// 连接成功 启动心跳功能
+		}
+		rtn = connect.sendWithEcho("player@getPlayerList", "sessionId=111");
 	}
 }
